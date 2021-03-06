d3dc413b44d5b809f55e94d4560a5ffb4242908e
Iryna <uaireneua@gmail.com>
2021-03-06 16:47:27 +0200

    Updated HeaderMenu. Added a block of user information

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -4,8 +4,22 @@
             <router-link to="/login">Вхід</router-link> /
             <router-link to="/registration">Реєстрація</router-link>
         </div>
-        <b-img :src="`${url1}${me.avatar_url}`"></b-img>
-        <b-button class="btn-form" variant="secondary" @click.prevent="logout" v-if="token">Вийти</b-button>
+        <div v-else>
+            <b-img class="user-avatar" :src="`${url1}${me.avatar_url}`"></b-img>
+            <div class="user-block">
+                <ul class="user-menu">
+                    <router-link to="/profile"><li>
+                        <a href="">Профіль</a>
+                    </li></router-link>
+                    <router-link to="/myposts"><li>
+                        <a href="">Мої статті</a>
+                    </li></router-link>
+                    <li>
+                        <a @click="logout" class="logout">Вийти</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -64,5 +78,51 @@ a:hover {
 }
 .header-menu a.router-link-exact-active {
     color: #fb7842;;
+}
+.user-avatar:hover ~ div{
+	display: block;
+}
+.user-block {
+    width: 200px;
+    height: 150px;
+    border: none;
+    border-radius: 15px;
+    position: absolute;
+    right: 106px;
+    margin-top: 24px;
+    background: linear-gradient(#fca468, #cec5f1);
+    display: none;
+    z-index: 1;
+    box-sizing: border-box;
+}
+.user-block:before {
+    position: absolute;
+    top: -25px;
+    left: 77px;
+    transform: rotate(360deg);
+    content: '';
+    border-style: solid;
+    border-width: 0 20px 25px 20px;
+    border-color: transparent transparent #f9a56f transparent;
+}
+.user-block:hover{
+	display: block;
+}
+.user-menu {
+    padding: 12px 25px;
+    line-height: 40px;
+    text-align: left;
+    font-size: 19px;
+}
+.user-menu li {
+   list-style: none; 
+}
+.user-menu li a, .logout {
+    color: white;
+}
+.user-menu li a:hover, .logout:hover {
+    color: #ffecac;
+    text-decoration: none;
+    cursor: pointer;
 }
 </style>
